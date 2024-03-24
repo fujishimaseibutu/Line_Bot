604c5a27e70af000337f9d79eb73e9d4a3a831ee
fujishimaseibutu <135817534+fujishimaseibutu@users.noreply.github.com>
2024-03-24 10:37:13 +0900

    Update message_handler.py

--- message_handler.py
+++ message_handler.py
@@ -18,7 +18,7 @@ DROPBOX_REFRESH_TOKEN=os.getenv('CHANNEL_REFRESH_TOKEN')
 # Dropboxのルートにこの名前のフォルダを事前に作っておく必要がある
 DROPBOX_ROOT = '/fujishima_weasel/'
 DROPBOX_IMAGE_ROOT='/fujishima_image/'
-client = dropbox.Dropbox(app_key='ng3r13wmw0k35g9',app_secret='abnb48hn78crx6p',oauth2_refresh_token='2fl8VXXfWdsAAAAAAAAAATAUF3oa2VqnRLvWV5E0HFsEgZhnYDIg-UYMjcPh6oIq')
+client = dropbox.Dropbox(app_key='j12zedwszd957i0',app_secret='	pjdv71jycvbcl23',oauth2_refresh_token='2fl8VXXfWdsAAAAAAAAAATAUF3oa2VqnRLvWV5E0HFsEgZhnYDIg-UYMjcPh6oIq')
 
 send_num=[0]
 
